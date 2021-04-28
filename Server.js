2f0141b6811b8a4d8d6fe2b58743d8f5bc6fc3ba
Dzirt2006 <dzirt-2006@mail.ru>
2021-04-27 22:13:55 -0400

    login fixed

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8000;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "shhh its a secret";
 
 
-app.use(express(json));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -17,22 +17,38 @@ const postData = [
         title: "post 1"
     },
     {
-        username: "Bob",
+        username: "Alex",
         title: "post 2"
     }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(postData);
+app.get('/posts', authToken, (req, res) => {
+    console.log(req.user)
+    res.json(postData.filter(post => post.username === req.user.name));
 })
 
-app.post('/login', (req, res) => {
-console.log(req.body)
+app.post('/login', (req, res) => {  
     const username = req.body.username;
     const user = { name: username };
+    console.log('user here!!! => ', user)
     const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
     res.json({ accessToken: accessToken });
 })
+
+
+function authToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1]; //if aHeader return ah.split | undefined
+    console.log(token)
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        console.log(user, ' ')
+        req.user = user;
+        next()//move on from middleware
+    })
+}
 
 
 app.listen(PORT, () =>
