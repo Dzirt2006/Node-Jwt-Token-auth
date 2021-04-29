a7cca4b4b6d8068ff146c7bbe09e535b3aaddbd1
Dzirt2006 <dzirt-2006@mail.ru>
2021-04-28 20:34:51 -0400

    two servers works great

--- authServer.js
+++ authServer.js
@@ -5,22 +5,41 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 4000;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "shhh its a secret";
-
+const RESRESH_TOKEN_SECRET = process.env.RESRESH_TOKEN_SECRET || "shhh its a BIGEST secret";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const refreshedTokens = [];
 
-app.post('/login', (req, res) => {  
+app.post('/token', (req, res) => {
+    console.log(refreshedTokens);
+    const refreshedToken = req.body.token;
+    if (refreshedToken == null) return res.sendStatus(401)
+    if (!refreshedTokens.includes(refreshedToken)) return res.sendStatus(403);
+    jwt.verify(refreshedToken, RESRESH_TOKEN_SECRET, (err, user) => {
+        if (err) res.sendStatus(403);
+        console.log(user)
+        const accessToken = genAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    })
+})
+
+//Authenticate user
+app.post('/login', (req, res) => {
     const username = req.body.username;
     const user = { name: username };
     console.log('user here!!! => ', user)
-    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
+    const accessToken = genAccessToken(user);
+    const refreshToken = jwt.sign(user, RESRESH_TOKEN_SECRET);
+    refreshedTokens.push(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
 })
 
 
-
+function genAccessToken(user) {
+    return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+}
 
 
 app.listen(PORT, () =>
